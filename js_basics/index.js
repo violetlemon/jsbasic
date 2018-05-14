7585ab465b7d059740c3df1e501df16188bbe2b0
Hui Tang <huihh.tang@gmail.com>
2018-05-13 20:39:08 -0400

    JS basic

---
+++ js_basics/index.js
@@ -0,0 +1,20 @@
+let name = 'Mosh'
+console.log(name)
+
+let person = {
+    name: 'H',
+    age: 30
+};
+
+console.log(person);
+
+let selectedConsole = ['red', 'blue'];
+console.log(selectedConsole);
+
+
+function greet(){
+    console.log('Hello here, yes ok');
+}
+
+greet();
+
